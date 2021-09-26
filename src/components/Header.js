a66419a64c590442727a20870ebc7c7b3575c19d
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-09-26 21:29:14 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// functional component
+const Header = () => {
+    return (
+        // Using libraries from semantic-ui
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
